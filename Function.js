e6ee69439f871bcea646188575ef6a107cb6593a
Monicaa21 <airenrin1122@gmail.com>
2026-03-11 19:52:28 +0700

    Upload fungsi dari monic

--- Function.js
+++ Function.js
@@ -7,3 +7,8 @@ KelilingPersegi = ((a) => {console.log(4 * a)});
 KelilingPersegi(10);
 
 //Punya Louise
+
+LuasSegitiga = ((a, t) => {console.log(0.5 * a * t)});
+LuasSegitiga(10, 20);
+
+//Punya Monic
