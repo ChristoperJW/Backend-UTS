67569b452e1e05ea8804d2e7d35bc82feb3b0ad6
Christoper <christoper1177@gmail.com>
2026-03-11 19:22:57 +0700

    Delete AreaofCircle.js

--- AreaofCircle.js
+++
@@ -1,9 +0,0 @@
-function AreaofCircle(radius) {
-    return Math.PI * radius * radius;
-}
-
-// const radius = 5;
-// const area = AreaofCircle(radius);
-// console.log(`The area of the circle is: ${area}`)
-
-console.log(`The area of the circle is ${AreaofCircle(10)}`);
